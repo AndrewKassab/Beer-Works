f2cf4aa0170f99c174b77b6e1741f380704853d7
andrewkassab <andrewkassab@live.com>
2024-10-03 18:43:11 -0700

    cascaded persistence

--- src/main/java/andrewkassab/spring/spring_6_rest_mvc/entity/BeerOrder.java
+++ src/main/java/andrewkassab/spring/spring_6_rest_mvc/entity/BeerOrder.java
@@ -28,7 +28,7 @@ public class BeerOrder {
         this.customerRef = customerRef;
         this.setCustomer(customer);
         this.beerOrderLines = beerOrderLines;
-        this.beerOrderShipment = beerOrderShipment;
+        setBeerOrderShipment(beerOrderShipment);
     }
 
     @Id
@@ -59,12 +59,17 @@ public class BeerOrder {
     @OneToMany(mappedBy = "beerOrder")
     private Set<BeerOrderLine> beerOrderLines;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.PERSIST)
     private BeerOrderShipment beerOrderShipment;
 
     public void setCustomer(Customer customer) {
         this.customer = customer;
         customer.getBeerOrders().add(this);
+    }
+
+    public void setBeerOrderShipment(BeerOrderShipment beerOrderShipment) {
+        this.beerOrderShipment = beerOrderShipment;
+        beerOrderShipment.setBeerOrder(this);
     }
 
 }
--- src/test/java/andrewkassab/spring/spring_6_rest_mvc/repository/BeerOrderRepositoryTest.java
+++ src/test/java/andrewkassab/spring/spring_6_rest_mvc/repository/BeerOrderRepositoryTest.java
@@ -2,16 +2,14 @@ package andrewkassab.spring.spring_6_rest_mvc.repository;
 
 import andrewkassab.spring.spring_6_rest_mvc.entity.Beer;
 import andrewkassab.spring.spring_6_rest_mvc.entity.BeerOrder;
+import andrewkassab.spring.spring_6_rest_mvc.entity.BeerOrderShipment;
 import andrewkassab.spring.spring_6_rest_mvc.entity.Customer;
 import jakarta.transaction.Transactional;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BeerOrderRepositoryTest {
@@ -40,6 +38,9 @@ class BeerOrderRepositoryTest {
         BeerOrder beerOrder = BeerOrder.builder()
                 .customerRef("Test order")
                 .customer(testCustomer)
+                .beerOrderShipment(BeerOrderShipment.builder()
+                        .trackingNumber("1235r")
+                        .build())
                 .build();
 
         BeerOrder savedBeerOrder = beerOrderRepository.save(beerOrder);
